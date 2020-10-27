a34e8eaecbce25d24e0afb27ca9de171052a5508
Laika-Beats <laikabeatsdenver@gmail.com>
2020-10-27 14:35:58 -0600

    delete notes code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-//Dependencies
+// Dependencies
 const express = require("express")
 const fs = require("fs")
 const path = require("path")
 
-//Sets up Express app
+// Sets up Express app
 const app = express()
 const PORT = 3000
 
@@ -13,56 +13,54 @@ app.use(express.json())
 // The following code serves static files in a directory named public:
 app.use(express.static('public')) 
 
-// Routes
-const publicDir = path.join(__dirname, "/public")
+// Converts text in the db.json in to Javascript objects
+let dbNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"))
 
+// Routes
 app.get("/notes", function(req, res) {
-    res.sendFile(path.join(publicDir, "notes.html"));
+    res.sendFile(path.join(__dirname, "public/notes.html"))
 });
 
 app.get("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/db/db.json"));
+    res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 
 app.get("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    res.json(savedNotes[req.params.id]);
+    
+    res.json(dbNotes[req.params.id])
 });
 
 app.get("*", function(req, res) {
-    res.sendFile(path.join(publicDir, "index.html"));
+    res.sendFile(path.join(__dirname, "public/index.html"))
 });
-
+// Saves notes to db.json
 app.post("/api/notes", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let newNote = req.body;
-    let uniqueID = (savedNotes.length).toString();
-    newNote.id = uniqueID;
-    savedNotes.push(newNote);
+    let newNote = req.body
+    let uniqueID = (dbNotes.length).toString()
+    newNote.id = uniqueID
+    dbNotes.push(newNote)
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    res.json(savedNotes);
-})
-
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbNotes))
+    res.json(dbNotes)
+});
+// Deletes notes in db.json
 app.delete("/api/notes/:id", function(req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-    let noteID = req.params.id;
-    let newID = 0;
-    savedNotes = savedNotes.filter(currNote => {
-        return currNote.id != noteID;
+    let noteID = req.params.id
+    let newID = 0
+    dbNotes = dbNotes.filter(function(currNote){
+        return currNote.id != noteID
     })
     
-    for (currNote of savedNotes) {
-        currNote.id = newID.toString();
-        newID++;
+    for (currNote of dbNotes) {
+        currNote.id = newID.toString()
+        newID++
     }
 
-    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-    res.json(savedNotes);
-})
+    fs.writeFileSync("./db/db.json", JSON.stringify(dbNotes))
+    res.json(dbNotes)
+});
 
 // Starts the server to begin listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT)
-})
-
+});
