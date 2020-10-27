149c1a5aeb4a113d7cbffdcc32863155a0126d7e
Laika-Beats <laikabeatsdenver@gmail.com>
2020-10-26 20:51:38 -0600

    display, save, and delete notes

--- server.js
+++ server.js
@@ -5,14 +5,64 @@ const path = require("path")
 
 //Sets up Express app
 const app = express()
-const port = 3000
+const PORT = 3000
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-// To serve static files such as images, CSS files, and JavaScript files, use the express.static built-in middleware function in Express.
 // The following code serves static files in a directory named public:
 app.use(express.static('public')) 
 
 // Routes
-// =============================================================
+const publicDir = path.join(__dirname, "/public")
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(publicDir, "notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.get("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    res.json(savedNotes[req.params.id]);
+});
+
+app.get("*", function(req, res) {
+    res.sendFile(path.join(publicDir, "index.html"));
+});
+
+app.post("/api/notes", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let newNote = req.body;
+    let uniqueID = (savedNotes.length).toString();
+    newNote.id = uniqueID;
+    savedNotes.push(newNote);
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+app.delete("/api/notes/:id", function(req, res) {
+    let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    let noteID = req.params.id;
+    let newID = 0;
+    savedNotes = savedNotes.filter(currNote => {
+        return currNote.id != noteID;
+    })
+    
+    for (currNote of savedNotes) {
+        currNote.id = newID.toString();
+        newID++;
+    }
+
+    fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
+    res.json(savedNotes);
+})
+
+// Starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT)
+})
+
